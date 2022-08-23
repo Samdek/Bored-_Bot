016fc53e251fcae553f884b10c9ec62031120fca
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2022-08-23 12:28:47 +0100

    final commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function getActivityIdea() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("idea").textContent = data.activity
+            document.body.classList.add("fun")
+            document.getElementById("title").textContent = " HappyBot"
+            document.getElementById("bored-button").classList.add("fun_btn")
+        })
+}
+
+document.getElementById("bored-button").addEventListener("click", getActivityIdea)
